f18078e2c039f98ed5902a23e5e67aad06bc6b47
Clayton Cooper <claytoncooper018@gmail.com>
2026-01-02 14:24:16 +0000

    Updating

---
+++ CShoppingCart/Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CShoppingCart.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
